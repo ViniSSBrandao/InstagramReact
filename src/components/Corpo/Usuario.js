6a9ac2c2dbfeca75b82645484444d17f88a80c28
Vinicius Brandao <vinicius.brandaot@gmail.com>
2022-11-21 12:26:54 -0300

    alterar nome e imagem

--- src/components/Corpo/Usuario.js
+++ src/components/Corpo/Usuario.js
@@ -1,15 +1,49 @@
+import React from "react";
 
 export default function Usuario(prop){
+    
+   
+    
+
+    const [imagem, setImagem] = React.useState(prop.imagem);
+    const [nome, setNome] = React.useState(prop.nome);
+
+   function novaImagem(){
+        const entrada = prompt("Escolha seu nome de usuario")
+        setImagem(entrada)
+   }
+
+    function novoNome(){
+        const entrada = prompt("Escolha seu nome de usuario")
+        setNome(entrada)
+    }
+    
+    
+
     return (
         <div class={"usuario"}>
-            <img src={prop.imagem} />
+            <img src={imagem} onClick={novaImagem} />
             <div class="texto">
             <strong>{prop.perfil}</strong>
             <span>
-                {prop.nome}
-                <ion-icon name="pencil"></ion-icon>
+                <Nome nome={nome}/>
+                <ion-icon name="pencil" onClick={novoNome}></ion-icon>
             </span>
         </div>  
         </div>
     );
+}
+
+
+
+function Nome(prop){
+return(<>{prop.nome}</>);
+}
+
+function clicouFoto(){
+    const novaFoto = {
+        imagem: prompt("insira link para sua nova foto de perfil")
+    }
+    
+    Usuario(novaFoto)
 }
